0d9d50e95ae88042fbd604a2f50f354cfa61e6ca
Allen Hai <allenhai03@gmail.com>
2019-04-11 23:02:20 -0500

    change cookie domain from localhost to vrs-git-auth.zeit.sh

--- backend/middlewares/cookieSession.js
+++ backend/middlewares/cookieSession.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
 const cookieSession = require("cookie-session");
 
 module.exports = cookieSession({
   name: "user-from-github",
   keys: [process.env.COOK_KEY],
-  domain: "localhost:3000",
+  domain: "vrs-git-auth.zeit.sh",
   maxAge: 24 * 60 * 60 * 1000
 });
