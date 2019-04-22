dbf1f53d991c06ab867fdd0de40b939965f0be8b
Allen Hai <allenhai03@gmail.com>
2019-04-22 11:07:00 -0500

    convert async/await req handler in checkout lambda to use promises

--- backend/api/checkout/index.js
+++ backend/api/checkout/index.js
@@ -1,10 +1,11 @@
 const app = require("../../utils/app");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-app.post("*", async (req, res) => {
+app.post("*", (req, res) => {
   const { amount, token } = req.body;
   console.log("AMOUNT:", amount);
   console.log("TOKEN:", token);
+  /*
   try {
     const { status } = await stripe.charges.create({
       amount: parseInt(amount),
@@ -16,6 +17,16 @@ app.post("*", async (req, res) => {
   } catch ({ message }) {
     res.status(400).json({ error: message || "uh oh " });
   }
+  */
+  stripe.charges
+    .create({
+      amount: parseInt(amount),
+      currency: "usd",
+      description: "VRS",
+      source: token
+    })
+    .then(({ status }) => res.json({ status }))
+    .catch(({ message }) => res.status(400).json({ error: message }));
 });
 
 module.exports = app;
