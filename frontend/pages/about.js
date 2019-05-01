4afc37f7fefd8fe9063de7d39704ba85423e94f5
Sarup Banskota <sbanskota08@gmail.com>
2019-05-02 04:03:27 +0800

    Remove questions

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -19,28 +19,25 @@ export default function About({
       toggleCartOpen={toggleCartOpen}
     >
       <main className="pa3 pa5-ns vh-100 white dt">
-        <div className="w-50">
-          <h2 className="f4 lh-copy">What is VRS?</h2>
+        <div className="items-center">
           <p className="lh-copy">
             VRS is a fully-functional e-commerce store, built on the serverless
             paradigm. With a single command `now`, the store deploys instantly,
             scales automatically, and requires zero supervision.
           </p>
-          <h2 className="f4 lh-copy">Can it handle high traffic?</h2>
           <p className="lh-copy">
-            Since VRS is deployed with
+            Since VRS is deployed with{' '}
             <a className="link dim white underline" href="https://zeit.co/now">
               Now
             </a>
             , the underlying infrastructure can handle peak time traffic with
-            consistently blazing fast performance. Thanks to the caching done on
+            consistent, blazing-fast performance. Thanks to the caching done on
             the expansive{" "}
             <a className="link dim white underline" href="https://zeit.co/cdn">
               ZEIT CDN network
-            </a>{" "}
-            customers located anywhere enjoy equally low latency.
+            </a>,
+            customers located anywhere can enjoy equally low latency.
           </p>
-          <h2 className="f4 lh-copy">Is it expensive?</h2>
           <p className="lh-copy">
             The source-code for VRS is completely free and{" "}
             <a
@@ -49,14 +46,16 @@ export default function About({
             >
               open source
             </a>
-            . Now has a generous free tier, which is sufficient to deploy VRS
-            and try it out. Thanks to the on-demand pricing model, for
+            . You can deploy VRS to our scalable serverless infrastructure{' '}
+            <strong>free of cost</strong>. Thanks to the on-demand pricing model, for
             production usage, owners of the store don’t pay a cent for
-            infrastructure during idle time. What’s more — thanks to serverless
+            infrastructure during idle time.
+            <br/>
+            <br/>
+            What’s more — thanks to serverless
             pre-rendering, even dynamic content can be served through cache,
             resulting in drastic savings on server bills.
           </p>
-          <h2 className="f4 lh-copy">How do I set it up?</h2>
           <p className="lh-copy">
             The code for this demo is completely open source. Once you have{" "}
             <a
@@ -82,7 +81,6 @@ export default function About({
             </a>
             explains how you can specify the secrets.
           </p>
-          <h2 className="f4 lh-copy">Why did you make this?</h2>
           <p className="lh-copy">
             Our mission at ZEIT is to make the cloud accessible to everyone. We
             do that by creating products that improve developer experience,
