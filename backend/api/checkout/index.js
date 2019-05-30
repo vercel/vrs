ff2e4760dabc72d0cdc85adea2c6c1a4feb6b381
Rob Brennan <rob@therobbrennan.com>
2019-05-29 21:03:16 -0700

    Using temporary Stripe tok_visa token

--- backend/api/checkout/index.js
+++ backend/api/checkout/index.js
@@ -3,13 +3,15 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 app.post("*", async (req, res) => {
   const { amount, token } = req.body;
-
+  const testToken = 'tok_visa'  // Use 4242 4242 4242 4242 Visa for testing
   try {
     const { status } = await stripe.charges.create({
       amount: parseInt(amount),
       currency: "usd",
       description: "VRS",
-      source: token
+      // source: token
+      // TODO: Uncomment the above code to use the live token returned from Stripe
+      source: testToken
     });
     res.json({ status });
   } catch ({ message }) {
