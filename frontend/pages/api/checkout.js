5cfb8530e87e72516261d8de85247bc366835ca2
Lydia Hallie <lydiajuliettehallie@gmail.com>
2022-03-01 11:13:11 -0700

    Add checkout API route

---
+++ frontend/pages/api/checkout.js
@@ -0,0 +1,18 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+export default async function handler(req, res) {
+  const { amount, token } = req.body;
+
+  try {
+    const { status } = await stripe.charges.create({
+      amount: parseInt(amount),
+      currency: "usd",
+      description: "VRS",
+      source: token
+    });
+
+    res.status(200).json({ status })
+  } catch ({ message }) {
+    res.status(400).json({ error: message });
+  }
+}
